e59db532cbb44d51354f204641e9fd33fdd6eacc
Abhishek M. Sharma <standup787059@gmail.com>
2025-03-26 22:54:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var btn = document.querySelector('button');
+
+var percent = document.querySelector('#percent');
+var growth = document.querySelector('#growth');
+
+var grow=0;
+btn.addEventListener('click', function(){
+     var int = setInterval(() => {
+          grow++
+          percent.innerHTML = grow+"%";
+          growth.style.width = grow+'%';
+     }, 50);
+
+     setTimeout(() => {
+          clearInterval(int);
+          btn.innerHTML = 'Downloaded';
+          btn.style.opacity = .5;
+          
+     },5000);
+})
